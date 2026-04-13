be6d72de24d9cd503c48f573d8ad41d15712a6eb
tkhy2651 <tah331925@gmail.com>
2026-04-13 21:33:19 +0900

    js

--- src/main.js
+++ src/main.js
@@ -4,20 +4,17 @@ import fragSrc from './shaders/base.frag'
 const canvas = document.getElementById('canvas')
 const gl = canvas.getContext('webgl')
 
-// canvasのサイズをウィンドウに合わせる
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 gl.viewport(0, 0, canvas.width, canvas.height)
 
-// シェーダーをコンパイルする
 function createShader(type, source) {
-  const shader = gl.createShader(type)
-  gl.shaderSource(shader, source)
-  gl.compileShader(shader)
-  return shader
+    const shader = gl.createShader(type)
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+    return shader
 }
 
-// プログラムを作る
 const vert = createShader(gl.VERTEX_SHADER, vertSrc)
 const frag = createShader(gl.FRAGMENT_SHADER, fragSrc)
 const program = gl.createProgram()
@@ -26,23 +23,36 @@ gl.attachShader(program, frag)
 gl.linkProgram(program)
 gl.useProgram(program)
 
-// 画面全体を覆う四角形の頂点
 const positions = new Float32Array([
-  -1, -1,
-   1, -1,
-  -1,  1,
-   1,  1,
+    -1, -1,
+    1, -1,
+    -1, 1,
+    1, 1,
 ])
 
-// GPUに頂点データを送る
 const buffer = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
 gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW)
 
-// シェーダーのposition変数とバッファを繋げる
 const loc = gl.getAttribLocation(program, 'position')
 gl.enableVertexAttribArray(loc)
 gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0)
+gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
 
-// 描画
-gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+// 画像を読み込んでテクスチャとしてGPUに送る
+const image = new Image()
+image.src = '/images/section1.jpg'
+image.onload = () => {
+    const texture = gl.createTexture()
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+
+    // シェーダーのuTextureにテクスチャを渡す
+    const uTexture = gl.getUniformLocation(program, 'uTexture')
+    gl.uniform1i(uTexture, 0)
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+}
