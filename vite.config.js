7ac45d3fe4cefe59c8efd0c816d32706b9e7757b
tkhy2651 <tah331925@gmail.com>
2026-04-15 21:19:26 +0900

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,19 @@ import glsl from 'vite-plugin-glsl'
 
 export default defineConfig({
     base: '/shader-website-03/',
+
     plugins: [
-        glsl({
-            watch: true,
-            compress: false,
-            defaultExtension: 'frag',
-        })
+        glsl()
     ],
+
     build: {
-        outDir: 'dist',
-    }
+        assetsInlineLimit: 0,
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`,
+            },
+        },
+    },
 })
