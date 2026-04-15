9817389bd461dd3a5f67e420c63daa1ed60224f3
tkhy2651 <tah331925@gmail.com>
2026-04-15 21:10:27 +0900

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import glsl from 'vite-plugin-glsl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/shader-website-03/', // ここをリポジトリ名に合わせて追加
+    base: '/shader-website-03/',
     plugins: [glsl()]
 })
