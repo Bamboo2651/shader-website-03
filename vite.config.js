646889f29ef2009f504cfd355890cade97c4fe98
tkhy2651 <tah331925@gmail.com>
2026-04-15 21:09:18 +0900

    公開github page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import glsl from 'vite-plugin-glsl'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [glsl()]
+    base: '/shader-website-03/', // ここをリポジトリ名に合わせて追加
+    plugins: [glsl()]
 })
